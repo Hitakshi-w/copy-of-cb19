9ce98227cf671218c780151fb07c1e89f593481f
Neetika Tandon <69705083+Neetika146@users.noreply.github.com>
2023-01-08 14:57:03 +0530

    changes

--- registration.php
+++ registration.php
@@ -1,21 +1,20 @@
 <?php
 
 session_start();
-header('location: login.html');
-$con = mysqli_connect('localhost', 'root', '','registrations');
 
-/*mysqli_select_db($con, 'registrations');*/
-$name = $_POST['Name'];
-$email= $_POST['Email'];
-$password = $_POST['Password'];
-$s = " select * from usertable where Name = '$name'";
+$con = mysqli_connect('localhost', 'root', '');
+
+mysqli_select_db($con, 'userregistrations');
+$name = $_POST['user'];
+$password = $_POST['password'];
+$s = " select * from usertable where name = '$name'";
 $result = mysqli_query($con, $s);
 $num = mysqli_num_rows($result);
 if($num == 1){
     echo " Already registered";
 }
 else{
-    $reg = "insert into usertable(Name, Email, Password) values ($name, $email, $password)";
+    $reg = "insert into usertable(name, password) values ($name, $password)";
     mysqli_query($con, $reg);
     echo " signed up";
 }
