bf487f72db9a5d4ba966035a01c543f677d91643
Neetika Tandon <69705083+Neetika146@users.noreply.github.com>
2023-01-08 15:43:48 +0530

    dd

--- registration.php
+++ registration.php
@@ -1,22 +1,24 @@
 <?php
 
 session_start();
-header('location: login.html');
-$con = mysqli_connect('localhost', 'root', '','registrations');
 
-/*mysqli_select_db($con, 'registrations');*/
-$name = $_POST['Name'];
-$email= $_POST['Email'];
-$password = $_POST['Password'];
-$s = " select * from usertable where Name = '$name'";
-$result = mysqli_query($con, $s);
-$num = mysqli_num_rows($result);
-if($num == 1){
-    echo " Already registered";
-}
-else{
-    $reg = "insert into usertable(Name, Email, Password) values ($name, $email, $password)";
-    mysqli_query($con, $reg);
-    echo " signed up";
-}
+$con = mysqli_connect('localhost', 'root', '','userregisteration');
+if(mysqli_connect_error())
+        echo "Connection Error.";
+    else
+        echo "Database Connection Successfully.";
+// mysqli_select_db($con, 'userregisteration');
+// $name = $_POST['user'];
+// $password = $_POST['password'];
+// $s = " select * from usertable where name = '$name'";
+// $result = mysqli_query($con, $s);
+// $num = mysqli_num_rows($result);
+// if($num == 1){
+//     echo " Already registered";
+// }
+// else{
+//     $reg = "insert into usertable(name, password) values ($name, $password)";
+//     mysqli_query($con, $reg);
+//     echo " signed up";
+// }
 ?>
